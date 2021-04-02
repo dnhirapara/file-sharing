ab1f300c57c892565f94b4252e12da7fe97dc6a7
dnhirapara <darshikhirapara@gmail.com>
2021-04-02 14:47:56 +0530

    bug fixes

---
+++ src/components/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Loader.css";
+
+function Loader() {
+  return (
+    <div>
+      <div class="loader" style={{ zIndex: 25 }}>
+        Loading...
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
