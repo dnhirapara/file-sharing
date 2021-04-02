40b27b1e2efde43f2163989042c3ffd068c53f8d
dnhirapara <darshikhirapara@gmail.com>
2021-04-02 16:09:50 +0530

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import FileList from "./components/FileList";
 import NotFound404 from "./components/NotFound404";
